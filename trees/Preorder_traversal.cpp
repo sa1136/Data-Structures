f7b2cc2ae719ad878ea7b748b0ff0e75a892785f
sa1136 <spakhilaa10@gmail.com>
2025-11-03 10:43:50 -0500

    Add Trees: Binary Tree Preorder Traversal\n\n- Recursive preorder traversal storing values\n- Includes TreeNode definition and test main

---
+++ trees/Preorder_traversal.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result; 
+        preorder(root, result);
+        return result;
+    }
+    
+private:
+    void preorder(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) return; // use 'nullptr' instead of 'null'
+        result.push_back(root->val); // store the value instead of print
+        preorder(root->left, result);
+        preorder(root->right, result);
+    }
+};
+
+int main() {
+    // Build a simple tree:    1
+    //                        / \
+    //                       2   3
+    //                      / \
+    //                     4   5
+    TreeNode* root = new TreeNode(1,
+        new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+        new TreeNode(3)
+    );
+
+    Solution sol;
+    vector<int> res = sol.preorderTraversal(root);
+    cout << "Preorder: ";
+    for (size_t i = 0; i < res.size(); ++i) {
+        cout << res[i];
+        if (i + 1 < res.size()) cout << ",";
+    }
+    cout << "\n";
+
+    return 0;
+}
